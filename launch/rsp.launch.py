9a124844ecb4b0f97121506356ae7d423f2d3eea
Rony Gabbai <97525727+rgabbai@users.noreply.github.com>
2023-05-17 17:55:49 +0300

    Update rsp.launch.py

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -6,7 +6,7 @@ from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
+from launch.substitutions import Command
 import xacro
 
 
